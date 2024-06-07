61249ebcc48f5e89bf0430c8df8a0b935c38b746
michellebakels <michelle.bakels@gmail.com>
2024-06-07 12:14:41 -0400

    og?

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
 			customCss: [
 				'./src/styles/custom.css',
 			],
+			components: {
+				Head: './src/components/head.astro',
+			},
 			sidebar: [
 				{
 					label: 'Developer Health OS',
