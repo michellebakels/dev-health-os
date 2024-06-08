7fe414648d02dba20e8298477afe2d8f37f38b34
michellebakels <michelle.bakels@gmail.com>
2024-06-08 08:55:11 -0400

    add logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Developer Health OS',
+			logo: {
+				src: './src/assets/logo.svg',
+			  },
 			social: {
 				"x.com": 'https://twitter.com/g2i_co',
 			},
