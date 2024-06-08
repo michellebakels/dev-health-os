d4edf3ee946cb2bb5fae2084ce59ec43d31d569e
michellebakels <michelle.bakels@gmail.com>
2024-06-08 08:35:33 -0400

    add new font

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
 			},
 			customCss: [
 				'./src/styles/custom.css',
+				'./src/fonts/font-face.css',
 			],
 			components: {
 				Head: './src/components/head.astro',
